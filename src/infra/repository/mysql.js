8f5056d45f9cdb635aa3c1e1f9376668f88daf67
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-29 14:26:22 -0300

    separação de arquivos

---
+++ src/infra/repository/mysql.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql2/promise')
+
+module.exports = class BancoDb {
+  async connect() {
+    if (this.connection && this.connection.state !== 'disconnected') {
+      return this.connection
+    }
+
+    this.connection = await mysql.createConnection('mysql://jacksonUser:123456@localhost:3306/smarkio')
+    // console.log('Conectou no MySQL!')
+    return this.connection
+  }
+
+  async disconnect() {
+    await this.connection.end()
+  }
+
+  async insertCustomer(customer) {
+    const bd = await this.connect()
+    const sql = 'INSERT INTO teste1(name,text) VALUES (?,?);'
+    const values = [customer.nome, customer.text]
+    return await bd.query(sql, values)
+  }
+
+  async selectCustomers() {
+    const conn = await this.connect()
+    const [rows] = await conn.query('SELECT * FROM teste1;')
+    return rows
+  }
+
+}
--- src/infra/repository/mysql.spec.js
+++ src/infra/repository/mysql.spec.js
@@ -1,34 +1,4 @@
-const mysql = require('mysql2/promise')
-
-class BancoDb {
-  async connect() {
-    if (this.connection && this.connection.state !== 'disconnected')      {
-      return this.connection
-    }
-
-    this.connection = await mysql.createConnection('mysql://jacksonUser:123456@localhost:3306/smarkio')
-    // console.log('Conectou no MySQL!')
-    return this.connection
-  }
-
-  async disconnect() {
-    await this.connection.end()
-  }
-
-  async insertCustomer(customer) {
-    const bd = await this.connect()
-    const sql = 'INSERT INTO teste1(name,text) VALUES (?,?);'
-    const values = [customer.nome, customer.text]
-    return await bd.query(sql, values)
-  }
-
-  async selectCustomers() {
-    const conn = await this.connect()
-    const [rows] = await conn.query('SELECT * FROM teste1;')
-    return rows
-  }
-
-}
+const BancoDb = require('./mysql')
 
 describe('MySql', () => {
   test('Should Connection', async () => {
