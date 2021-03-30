e83db5a49e525bf65efe8cd140597932348a5eb9
JacksonECO <jacksongdas.jackson@gmail.com>
2021-03-29 23:08:33 -0300

    renomear arquivo do server nodejs e criação da roda de adicionar comentários

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const BancoDb = require('./infra/repository/mysql')
+
+let app = express()
+app.use(cors());
+let urlEncodedParser = bodyParser.urlencoded({extended:false})
+
+app.post('/api/addComment', urlEncodedParser, function async(req, res) {
+  const db = new BancoDb()
+
+  db.insertCustomer({
+    name: 'nome',
+    text: req.body.name,
+  })
+  console.log("jgfddfg")
+  let obj = {
+    name: req.body.name,
+    age: 18
+  }
+  res.json(obj);
+  db.disconnect()
+})
+
+app.get('/test', (req, res) => {
+  return res.send('Hello Worlds')
+})
+
+app.listen(3333, (req, res) => {
+  console.log(' Rodando na porta 3333')
+})
